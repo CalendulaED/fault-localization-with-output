7a24a5b6c85a76eacb9429829683d6733e47b6e8
Awu <68514251+CalendulaED@users.noreply.github.com>
2021-11-09 18:08:50 -0500

    Update localization.py

--- fault_localization_output/localization.py
+++ fault_localization_output/localization.py
@@ -2,6 +2,7 @@
 
 
 import collections
+import math
 
 
 ExecutionCounts = collections.namedtuple(
@@ -16,11 +17,23 @@ LINE_EXECUTIONS = collections.defaultdict(
     )
 )
 
+LINE_NOT_EXECUTIONS = collections.defaultdict(
+    lambda: ExecutionCounts(
+        positive_cases=0,
+        negative_cases=0,
+    )
+)
+
 
 def update_executions(lines, failed, line_executions=LINE_EXECUTIONS):
     """Update line executions to reflect test results."""
     for line in lines:
+        # print("Current line: ")
+        # print(line)
+        
         prev_executions = line_executions[line]
+        # print("prev_executions: ")
+        # print(prev_executions)
         line_executions[line] = ExecutionCounts(
             positive_cases=prev_executions.positive_cases + int(not failed),
             negative_cases=prev_executions.negative_cases + int(failed)
@@ -33,8 +46,9 @@ PRIOR = ExecutionCounts(
 )
 
 
-def calc_scores(line_executions=LINE_EXECUTIONS, prior=PRIOR):
+def calc_scores(failed, line_executions=LINE_EXECUTIONS, prior=PRIOR):
     """Return 'fault' score for each line, given prior and observations."""
+    """This is the original method, I think it is Jaccard"""
     return {
         line: float(
             execution_counts.negative_cases + prior.negative_cases
@@ -44,3 +58,61 @@ def calc_scores(line_executions=LINE_EXECUTIONS, prior=PRIOR):
         )
         for line, execution_counts in line_executions.items()
     }
+
+# def calc_scores(failed, line_executions=LINE_EXECUTIONS, prior=PRIOR):
+#     """Return 'fault' score for each line, given prior and observations."""
+#     """McCon method"""
+#     return {
+#         line: float(
+#             (execution_counts.negative_cases**2) - (failed - execution_counts.negative_cases)*execution_counts.positive_cases
+#         ) / (
+#             (execution_counts.negative_cases + (failed - execution_counts.negative_cases))*(execution_counts.negative_cases * execution_counts.positive_cases+prior.positive_cases)
+#         )
+#         for line, execution_counts in line_executions.items()
+#     }
+
+# def calc_scores(failed, line_executions=LINE_EXECUTIONS, prior=PRIOR):
+#     """Return 'fault' score for each line, given prior and observations."""
+#     """Kulczynski2 method test"""
+#     return {
+#         line: float(
+#             0.5
+#         ) * (
+#             (execution_counts.negative_cases)
+#             /(execution_counts.negative_cases + (failed - execution_counts.negative_cases)) 
+#             +
+#             (execution_counts.negative_cases)
+#             /(execution_counts.negative_cases + execution_counts.positive_cases)
+#         )
+#         for line, execution_counts in line_executions.items()
+#     }
+
+# def calc_scores(failed, line_executions=LINE_EXECUTIONS, prior=PRIOR):
+#     """Return 'fault' score for each line, given prior and observations."""
+#     """Ochiai, [hightlight the buggy lines]"""
+#     return {
+#         line: float(
+#             execution_counts.negative_cases
+#         ) / (
+#             math.sqrt((execution_counts.negative_cases + failed-execution_counts.negative_cases) * 
+#             (execution_counts.negative_cases + execution_counts.positive_cases))
+#         )
+#         for line, execution_counts in line_executions.items()
+#     }
+
+# def calc_scores(failed, line_executions=LINE_EXECUTIONS, prior=PRIOR):
+#     """Return 'fault' score for each line, given prior and observations."""
+#     """Zoltar version [very very highlight the buggy lines]"""
+#     # print(math.sqrt((execution_counts.negative_cases +prior.negative_cases) * (execution_counts.positive_cases + prior.negative_cases)))
+#     return {
+#         line: float(
+#             execution_counts.negative_cases
+#         ) / (
+#             execution_counts.negative_cases + (failed - execution_counts.negative_cases) + execution_counts.positive_cases + 10000 * (
+#                 (failed - execution_counts.negative_cases) * execution_counts.positive_cases
+#             ) / (
+#                 execution_counts.negative_cases
+#             )
+#         )
+#         for line, execution_counts in line_executions.items()
+#     }
